2d247dcecda33894ba045169866e92a9c19d1413
dalz <dalz@alsd.eu>
2025-02-19 15:18:52 +0100

    working base model integration tests

--- shell.nix
+++ shell.nix
@@ -9,17 +9,29 @@ pkgs.mkShell {
     zlib
     z3
     ruby
+    git
 
     ocamlPackages.sail
 
     python312Packages.psutil
   ];
 
-  shellHook = "eval $(opam env --switch=.)";
+  shellHook = ''
+    eval $(opam env --switch=.)
+    export PATH="$(pwd)/dep/msp430-gcc-9.3.1.11_linux64/bin:$PATH"
+    export C_INCLUDE_PATH="$(pwd)/dep/msp430-gcc-support-files/include:$C_INCLUDE_PATH"
+  '';
 }
 
-# opam install sail
-# install katamaran sail backend
+# opam switch create .
+# cd dep/sail
+# (apply patch)
+# opam pin add sail .
+# (if not committed) opam reinstall sail --working-dir
+# cd ../sail-backend
+# opam install . --deps-only
+# dune build
+# dune install
 
 
-# (load "/home/ale/uni/tesi/_opam/.opam-switch/sources/sail.0.18/editors/sail-mode.el")
+# (load "/home/ale/uni/tesi/_opam/.opam-switch/sources/sail/editors/sail-mode.el")
