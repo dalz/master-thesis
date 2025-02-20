9be3df4047114a0eaf951f68e5918bf287d6d6ce
dalz <dalz@alsd.eu>
2025-02-20 17:50:00 +0100

    add unit tests for ipe

--- shell.nix
+++ shell.nix
@@ -14,6 +14,12 @@ pkgs.mkShell {
     ocamlPackages.sail
 
     python312Packages.psutil
+
+
+    # coq
+    # coqPackages.stdpp
+    # coqPackages.equations
+    # coqPackages.iris
   ];
 
   shellHook = ''
@@ -24,14 +30,37 @@ pkgs.mkShell {
 }
 
 # opam switch create .
+
+# opam repo add coq-released https://coq.inria.fr/opam/released
+# opam pin add coq 8.17.1   # latest release supported by katamaran
+
 # cd dep/sail
 # (apply patch)
 # opam pin add sail .
 # (if not committed) opam reinstall sail --working-dir
+
 # cd ../sail-backend
 # opam install . --deps-only
 # dune build
 # dune install
 
+# cd ../stdpp
+# git checkout a6387775  # needed by iris 4.1.0
+# (fix make-package shebang)
+# opam pin add coq-stdpp .
+# opam reinstall coq-stdpp --working-dir
+
+# cd ../iris
+# git checkout iris-4.1.0  # last release supporting coq 8.18 and supported by katamaran
+# opam pin add coq-iris .
+# (fix make-package shebang)
+# opam reinstall coq-iris --working-dir
+
+# opam repo add katamaran https://github.com/katamaran-project/opam-repository.git
+# opam install coq-katamaran
+
 
 # (load "/home/ale/uni/tesi/_opam/.opam-switch/sources/sail/editors/sail-mode.el")
+
+
+# opam repo add iris-dev https://gitlab.mpi-sws.org/iris/opam.git
