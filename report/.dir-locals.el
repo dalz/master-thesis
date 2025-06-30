57ca517e30c34230fac3d935a5269edaba461a12
dalz <dalz@alsd.eu>
2025-06-30 14:39:08 +0200

    report: extended contributions in introduction, spell checking

---
+++ report/.dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  . ((eglot-workspace-configuration
+      . (:ltex
+         (:language "en-US"
+          :additionalRules (:motherTongue "it")
+          ;; :dictionary (:en-US [":/home/ale/uni/tesi/report/dict.txt"])
+          ;; :latex (:commands (:\msp "dummy"
+          ;;                    :\usail "dummy"
+          ;;                    :\ie "dummy"
+          ;;                    :\eg "dummy")
+                            )))))))
+
